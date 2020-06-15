40e6d91f09707fc5cffc4364e8d6daaa432554dd
Max Wagner <maxwag56@gmail.com>
2020-06-14 21:13:58 -0600

    Move plex config out of plex mod to config
    The goal is to test Plex a little. Stubbing env vals seems impossible for testing.
    So moving this out makes it so the Plex struct can be filled out in the lib instead of inside itself.

--- src/config.rs
+++ src/config.rs
@@ -8,4 +8,21 @@ pub struct Config {
     /// Search case insensitive
     #[structopt(short, long)]
     pub ignore_case: bool,
+
+
+    #[structopt(long, env = "PLEX_TOKEN", hide_env_values = true)]
+    pub plex_token: String,
+
+    #[structopt(long, env = "PLEX_HOSTNAME", hide_env_values = true)]
+    pub plex_hostname: String,
+
+
+    #[structopt(long, env = "PLEX_PORT", default_value = "32400")]
+    pub plex_port: String,
+
+    #[structopt(long, env = "PLEX_GUIDE_DATA_CACHE", default_value = "/var/tmp/plex_guide_data_cache")]
+    pub plex_guide_data_cache: String,
+
+    #[structopt(long, env = "PLEX_ENABLE_GUIDE_DATA_CACHE")]
+    pub plex_enable_guide_data_cache: bool,
 }
